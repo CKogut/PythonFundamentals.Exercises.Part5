f62f69456e88082f33ffb2babe9c5a7d4e584c0a
Corinne <CorinneKogut@gmail.com>
2023-11-08 21:33:36 -0500

    Finished anagram

--- anagram.py
+++ anagram.py
@@ -3,17 +3,15 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     Given two strings, this functions determines if they are an anagram of one another.
     """
     count = 0
-
-    #check that they are the same length
-    if len(first_string) != len(second_string):
-      return False
     
     for letter in first_string:
-        if letter in first_string:
+        if letter in second_string:
             count +=1
 
     if count == len(first_string):
         return True
+    else:
+        return False
     
 
 
